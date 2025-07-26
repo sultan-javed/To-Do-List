6f9828c7bd29d11e8da9211e0ba4a02da43167f4
sultan-javed <sultanjavedm@gmail.com>
2025-07-26 10:48:13 +0530

    Add initial HTML, CSS, and JavaScript files for To-Do List application

---
+++ main.js
@@ -0,0 +1,66 @@
+// user can create their own to-do list
+// user delete tasks
+// search tasks
+// upadate tasks as completed
+
+let tasks = []
+
+let resultelement = document.getElementById('result');
+
+let renderTasks = (array) => {
+    resultelement.innerHTML = '';
+    array.forEach((item, index) => {
+        resultelement.innerHTML += `<div>
+                                      <h1><input type="checkbox" onchange="toggleCompleted(${item.id})" ${item.iscompleted ? 'checked' : ''}>${item.name} - ${item.iscompleted ? 'Completed' : ''}</h1>
+                                      <button onclick="removeTask(${item.id})">Remove</button>
+                                     </div>`;
+    });
+}
+
+document.getElementById('addbtn').addEventListener('click', () => {
+
+    // get the value from the input field
+
+    let value = document.getElementById('taskinput').value;
+    let object = {id: Date.now(), name: value, iscompleted: false};
+
+
+    //push the value into the tasks array
+
+    tasks.push(object);
+
+    //append the tasks to the result element
+
+    renderTasks(tasks);
+
+})
+
+
+let removeTask = (taskId) => {
+    // remove the task from the tasks array
+    tasks = tasks.filter(item => item.id != taskId);
+    renderTasks(tasks);
+}
+
+document.getElementById('searchinput').addEventListener('keyup', () => {
+
+    let searchkey = document.getElementById('searchinput').value
+
+    let searchedtasks = tasks.filter(item => {
+        return item.name.includes(searchkey);
+    })
+
+    renderTasks(searchedtasks);
+})
+
+let toggleCompleted = (taskId) => {
+    tasks=tasks.map(item => {
+        if (item.id === taskId) {
+            item.iscompleted = !item.iscompleted;
+            return item;
+        }else{
+            return item;
+        } 
+    });
+    renderTasks(tasks);
+}
